d693360549bfb34a0e5b5e490b1a08ae03967448
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-02-15 23:44:52 +0100

    STAGE 2

--- App/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class Book(BaseModel):
-    id: int
-    title: str
-    author: str
-    description: Optional[str] = None
-    price: float
